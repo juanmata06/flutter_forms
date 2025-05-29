f0a79adb5257410d8197f31488fe42dce77f678b
juanmata06 <mataguzzojuan@gmail.com>
2025-05-29 18:37:59 +0200

    bloc exercise working

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -14,11 +14,13 @@ class HomeScreen extends StatelessWidget {
         children: [
           ListTile(
             title: const Text('Cubits'),
+            subtitle: const Text('Gestor de estado simple'),
             trailing: const Icon(Icons.arrow_forward_ios_rounded),
             onTap: () => context.push('/cubits'),
           ),
           ListTile(
             title: const Text('Bloc'),
+            subtitle: const Text('Gestor de estado compuesto'),
             trailing: const Icon(Icons.arrow_forward_ios_rounded),
             onTap: () => context.push('/bloc'),
           )
